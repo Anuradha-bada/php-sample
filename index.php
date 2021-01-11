2d52c33f5964ac8a30eb563e3f134cbcffcd2d60
Anuradha-bada <59429885+Anuradha-bada@users.noreply.github.com>
2021-01-11 13:45:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ include 'web.config';
 include 'config.php'
 echo "$var1";
 echo "$var2";
-echo "ConfigurationSettings.AppSettings["var4"]"
+echo "$var4";
+#echo "ConfigurationSettings.AppSettings["var4"]"
 echo "HI azure app ";
 #echo " DEVOPS ";
 #echo "getenv("$var3")";
