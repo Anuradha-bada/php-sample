32ec585792259bd968d51b263059aae902db2e3f
Anuradha-bada <59429885+Anuradha-bada@users.noreply.github.com>
2021-01-12 09:45:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 include 'config.php';
-echo "$var1";
+echo "printing variabe1  $var1";
 echo "</br>"
-echo "$var2";
-echo "$var3";
-#echo "ConfigurationSettings.AppSettings["var4"]"
-echo "HI azure app ";
+echo "printing variable2 $var2";
 #echo " DEVOPS ";
 #echo "getenv("$var3")";
 echo getenv("var3");
 echo getenv("var2");
-echo $var2;
