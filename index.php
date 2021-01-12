79c84c2b9a8e7de240fce32b49fc8c8109198fa9
Anuradha-bada <59429885+Anuradha-bada@users.noreply.github.com>
2021-01-12 09:56:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 include 'config.php';
-echo "printing variabe1" . $var1;
+echo "printing variabe1&nbsp&nbsp" . $var1;
 echo "</br>";
-echo "printing variable2" . $var2 . "</br>";
+echo "printing variable2&nbsp&nbsp" . $var2 . "</br>";
 #echo " DEVOPS ";
 #echo "getenv("$var3")";
-echo getenv("var3");
+echo getenv("var1");
+echo "</br>";
 echo getenv("var2");
